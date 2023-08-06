009a3cf21b5afda02cd9608f455db56221689dba
Andresr35 <andresjruiz@cpp.edu>
2023-08-06 16:39:20 -0700

    delete last guide to get ready for next step. Start Output Management

--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
 import _ from 'lodash';
-import myName from './myName';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
-import Notes from './data.csv';
+
 
 function component(){
     const element = document.createElement('div');
 
     element.innerHTML = _.join(["hello",'webpack'],' ');
-    element.textContent = myName('Andres');
-    element.classList.add('hello');
 
-    const myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
-
-    console.log(Data);
-    console.log(Notes);
     
     return element;
 
