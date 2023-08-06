3cc44169ac93285ec99853c8968bad17435b620a
Andresr35 <andresjruiz@cpp.edu>
2023-08-06 15:35:13 -0700

    add css to webpack

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import _ from 'lodash';
 import myName from './myName';
+import './style.css'
 
 function component(){
     const element = document.createElement('div');
 
     element.innerHTML = _.join(["hello",'webpack'],' ');
     element.textContent = myName('Andres');
+    element.classList.add('hello');
     
     return element;
 
