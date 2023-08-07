9d10989211f0c0333117d96895b6af9264fe598f
Andresr35 <andresjruiz@cpp.edu>
2023-08-06 17:01:15 -0700

    add a webpack watcher

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,27 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');   
 
 module.exports={
+    mode:'development',
     entry:{
         index:'./src/index.js',
         print:'./src/print.js',
     },
+    devtool:'inline-source-map',
+    devServer:{
+        static:'./dist',
+    },
     plugins:[
         new HtmlWebpackPlugin({
-            title:'Output Management',
+            title:'Development',
         }),
     ],
     output:{
         filename:'[name].bundle.js',
         path:path.resolve(__dirname,'dist'),
         clean:true,
+    },
+    optimization:{
+        runtimeChunk:'single',
     },
 
 };
